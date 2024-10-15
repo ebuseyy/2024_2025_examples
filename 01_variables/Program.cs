91ab4862d71e1ef83066d80ab6e47c800892acbe
ebuseyy <eseyyarer@gmail.com>
2024-10-15 10:21:32 +0300

    Proje dosyası ekle.

---
+++ 01_variables/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace _01_variables
+{
+    class degerlendirme{
+        public void deger(int d)
+        {
+            if (d<100)
+            {
+                Console.WriteLine("değerimiz 100 den küçüktür");
+            }
+            else
+            {
+                Console.WriteLine("değerimiz 100 den büyüktür");
+            }
+        }
+    }
+    internal class Program
+    {
+        static int islem(int a,int b,char c)
+        {
+            int sonuc=0;
+            if (c == '+')
+                sonuc = a + b;
+            else if (c == '-')
+                sonuc = a - b;
+            else if (c == '*')
+                sonuc = a * b;
+            else if (c == '/')
+                sonuc = a / b;
+            else
+                Console.WriteLine("Hatalı Sembol");
+
+            return sonuc;   
+        }
+        static void Main(string[] args)
+        {
+            int a=0, b;
+            char c;
+            
+            Console.WriteLine("Lütfen iki sayı giriniz:");
+            try
+            {
+                a = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Lütfen sayı giriniz:");
+            }
+           
+
+            b = Convert.ToInt32(Console.ReadLine());
+
+            c = Convert.ToChar(Console.ReadLine());
+
+
+            int sonuc = islem(a, b, c);
+            degerlendirme o1=new degerlendirme();
+
+            degerlendirme2 o2 = new degerlendirme2();
+
+            o2.deger(sonuc);
+            o1.deger(sonuc);
+        }
+    }
+}
+
---
+++ 01_variables/degerlendirme2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_variables
+{
+    internal class degerlendirme2
+    {
+        public void deger(int d)
+        {
+            if (d < 100)
+            {
+                Console.WriteLine("değerimiz 100 den küçüktür");
+            }
+            else
+            {
+                Console.WriteLine("değerimiz 100 den büyüktür");
+            }
+        }
+    }
+}
